6ca1b59c75d5bd18cf8bd20b1ebd66b52dbe14f3
Paul Vorbach <paul@vorba.ch>
2016-01-12 01:10:04 +0100

    Standalone spring-boot with embedded Undertow

--- src/main/java/de/vorb/wildfly_springboot/StaticWebApplication.java
+++ src/main/java/de/vorb/wildfly_springboot/StaticWebApplication.java
@@ -2,19 +2,12 @@ package de.vorb.wildfly_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StaticWebApplication extends SpringBootServletInitializer {
+public class StaticWebApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(StaticWebApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(getClass());
     }
 
 }
